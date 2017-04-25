4b76f80b9344ccec3427e3ed73b00bf48b4990a4
Antoine Racine <aracine@macbook-2.local>
2017-04-25 10:26:47 -0400

    iTerm on ctrl + return

---
+++ init.lua
@@ -0,0 +1,21 @@
+-- TODO: Move this to a module 
+-- Make iTerm take half the screen (from top) on the main screen
+-- Press again to hide
+hs.hotkey.bind({"ctrl"}, "return", function()
+  local term = hs.application.find("iTerm2")
+  local termWin = term:mainWindow()
+  local termFrame = termWin:frame()
+  local screen = hs.screen.allScreens()[1]
+  local max = screen:frame()
+
+  if term:isFrontmost() then
+    term:hide()
+  else
+    termFrame.x = 0
+    termFrame.y = 0
+    termFrame.w = max.w
+    termFrame.h = max.h / 2
+    termWin:setFrame(termFrame)
+    termWin:focus()
+  end
+end)
