e1f345533f8b9cf509ac50dce6d32d681c54dfce
Antoine Racine <aracine@mirego.com>
2017-04-27 15:52:12 -0400

    Add todos

--- init.lua
+++ init.lua
@@ -1,8 +1,15 @@
 -- TODO: Move this to a module 
 -- Make iTerm take half the screen (from top) on the main screen
 -- Press again to hide
+-- TODO: Make it start iTerm if not already started NOT WORKING
+-- TODO: Spawn a window if iTerm is started with no window
 hs.hotkey.bind({"ctrl"}, "return", function()
   local term = hs.application.find("iTerm2")
+
+  if term == nil then
+       term = hs.application.open("iTerm", 0, true)
+  end
+
   local termWin = term:mainWindow()
   local termFrame = termWin:frame()
   local screen = hs.screen.allScreens()[1]
@@ -20,6 +27,7 @@ hs.hotkey.bind({"ctrl"}, "return", function()
   end
 end)
 
+-- TODO: Move this to a module
 -- Lock screen
 hs.hotkey.bind({"ctrl"}, "L", function()
   hs.caffeinate.lockScreen()
